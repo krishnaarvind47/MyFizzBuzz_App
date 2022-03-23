baed15caab3bb3a570c84549c5493cd0782fa85c
krishnaarvind47 <krishnaarvind47@gmail.com>
2022-03-23 09:31:19 -0500

    Add project files.

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,29 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public static string GetOutput(int number)
+        {
+            string output;
+
+            if ((number % 3 == 0) && (number % 5 == 0))
+            {
+                output = "FizzBuzz";
+            }
+            else if (number % 3 == 0)
+            {
+                output = "Fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                output = "Buzz";
+            }
+            else
+            {
+                output = number.ToString();
+            }
+
+            return output;
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(FizzBuzz.GetOutput(i));
+            }
+
+            Console.WriteLine("Press enter to quit...");
+            Console.ReadLine();
+        }
+    }
+}
